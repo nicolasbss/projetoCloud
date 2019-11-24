e4a9d66d95513c8ba5058a76d7d489faeb810d8a
Nicolas <nicolasbs@al.insper.edu.br>
2019-11-24 12:27:25 -0300

    Add pymongo connection

--- aps.py
+++ aps.py
@@ -1,6 +1,14 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, fields, marshal
 import os
+import pymongo
+
+client = pymongo.MongoClient(
+    "mongodb://52.87.213.151:27017")  # defaults to port 27017
+
+db = client['Projeto-Cloud']
+taskCollection = db['tasks']
+# taskCollection.drop()
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,21 +20,6 @@ task_fields = {
     'done': fields.Boolean,
     'uri': fields.Url('task')
 }
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
 
 
 class TaskListAPI(Resource):
@@ -40,16 +33,21 @@ class TaskListAPI(Resource):
         super(TaskListAPI, self).__init__()
 
     def get(self):
+        tasks = list(taskCollection.find())
         return {'tasks': [marshal(task, task_fields) for task in tasks]}
 
     def post(self):
         args = self.reqparse.parse_args()
+        tasks = list(taskCollection.find())
         task = {
             'id': tasks[-1]['id'] + 1 if len(tasks) > 0 else 1,
             'title': args['title'],
             'description': args['description'],
             'done': False
         }
+        taskCollection.insert_one(
+            task
+        )
         return {'task': marshal(task, task_fields)}, 201
 
 
@@ -63,27 +61,30 @@ class TaskAPI(Resource):
         super(TaskAPI, self).__init__()
 
     def get(self, id):
+        tasks = list(taskCollection.find())
         task = [task for task in tasks if task['id'] == id]
         if len(task) == 0:
             return {'result': '404'}
         return {'task': marshal(task[0], task_fields)}
 
     def put(self, id):
+        tasks = list(taskCollection.find())
         task = [task for task in tasks if task['id'] == id]
         if len(task) == 0:
             return {'result': '404'}
-        task = task[0]
         args = self.reqparse.parse_args()
+        print(args)
         for k, v in args.items():
             if v is not None:
-                task[k] = v
+                taskCollection.update_one({"id": id}, {"$set": {k: v}})
         return {'task': marshal(task, task_fields)}
 
     def delete(self, id):
+        tasks = list(taskCollection.find())
         task = [task for task in tasks if task['id'] == id]
         if len(task) == 0:
             return {'result': '404'}
-        tasks.remove(task[0])
+        taskCollection.delete_one({'id': id})
         return {'result': True}
 
 
