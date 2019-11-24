793fe996de1477f14ff76c0fa7f9e58f4b246b89
Nicolas <nicolasbs@al.insper.edu.br>
2019-11-24 16:13:26 -0300

    private ip env var

--- aps.py
+++ aps.py
@@ -3,8 +3,10 @@ from flask_restful import Api, Resource, reqparse, fields, marshal
 import os
 import pymongo
 
+mongoServerIp = os.environ["mongoIp"]
+
 client = pymongo.MongoClient(
-    "mongodb://ip-172-31-90-248.ec2.internal:27017")  # defaults to port 27017
+    "mongodb://"+mongoServerIp+":27017")  # defaults to port 27017
 
 db = client['Projeto-Cloud']
 taskCollection = db['tasks']
